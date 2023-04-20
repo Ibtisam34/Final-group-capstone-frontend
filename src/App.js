3e0817f57a46a5cfda8b2582d2eff362eb3052c0
odaialazzeh <odai.alazzeh@gmail.com>
2023-04-20 16:52:36 +0200

    Update the routs

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
-import './App.css';
 import React, { useState } from 'react';
-import AppRouter from './routers';
-import Header from './components/Header';
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './components/Homepage/Home';
+import Homepage from './components/Homepage/Homepage';
+import Detail from './components/Details';
+import Login from './components/LoginRegister/Login';
+import Register from './components/LoginRegister/Register';
+import AddDoctor from './components/AddDoctor/AddDoctor';
+import Auth from './components/LoginRegister/Auth';
 
 function App() {
-  const [currUser, setCurrUser] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(localStorage.getItem('isLoggedIn'));
+  const paths = Auth(isLoggedIn);
+
   return (
-    <div className="row">
-      <Header currUser={currUser} setCurrUser={setCurrUser} />
-      <div className="col-md-10 offset-md-2 main-content">
-        <AppRouter currUser={currUser} setCurrUser={setCurrUser} />
-      </div>
-    </div>
+    <main className="App">
+      <Routes>
+        <Route path="/*" element={<Home isLoggedIn={isLoggedIn} />}>
+          <Route exact path={paths.home} element={<Homepage isLoggedIn={isLoggedIn} />} />
+          <Route path={paths.details} element={<Detail />} />
+        </Route>
+        <Route
+          path={paths.AddDoctor}
+          element={isLoggedIn ? <AddDoctor /> : <Login setIsLoggedIn={setIsLoggedIn} />}
+        />
+        <Route path={paths.login} element={<Login setIsLoggedIn={setIsLoggedIn} />} />
+        <Route path={paths.register} element={<Register />} />
+      </Routes>
+    </main>
   );
 }
 
