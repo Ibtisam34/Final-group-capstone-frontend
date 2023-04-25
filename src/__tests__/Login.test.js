ebbeb98fa29c3ad3291322b41134d7af533854db
Tamunokuro <tamunokrobluejack@gmail.com>
2023-04-25 16:59:23 +0100

    Add test for Login

---
+++ src/__tests__/Login.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import store from '../redux/configureStore';
+import Login from '../components/LoginRegister/Login'
+
+
+describe('Login to app', () => {
+  it('renders correctly', () => {
+    const tree = render(<Provider store={store}><Router><Login /></Router></Provider>);
+    expect(tree).toMatchSnapshot();
+  });
+});
