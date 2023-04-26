cadffb30dc6de513f8de2f986cd3bb3a483afdeb
Tamunokuro <tamunokrobluejack@gmail.com>
2023-04-26 09:06:25 +0100

    Add appointment page snapshot

---
+++ src/components/_tests_/components/Appointment.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import store from '../../../redux/configureStore';
+import Appointment from '../../Appointments/Appointments';
+
+describe('Appointment booking page renders correctly', () => {
+    it('render the appointment page correctly', () => {
+        window.localStorage.setItem('userId', 1);
+        const tree = render(
+            <Provider store={store}>
+                <Router>
+                    <Appointment />
+                </Router>
+            </Provider>
+        );
+        expect(tree).toMatchSnapshot();
+    });
+})
