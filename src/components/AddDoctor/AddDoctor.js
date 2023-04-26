af5e239f40ebe8efb67ee08f55213fe0217bbb45
unknown <Riyanagwl@gmail.com>
2023-04-26 21:40:11 +0300

    add back button on the doctor form

--- src/components/AddDoctor/AddDoctor.js
+++ src/components/AddDoctor/AddDoctor.js
@@ -1,27 +1,28 @@
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
-import { Container } from 'react-bootstrap';
+import { Container, Button } from 'react-bootstrap';
 import CreateDoctorForm from './DoctorForm';
 import Response from './Response';
-
 function AddDoctor() {
   const doctors = useSelector((state) => state.doctorsReducer);
   const [show, setShow] = useState(true);
-
+  const handleGoBack = () => {
+    window.history.back();
+  };
   return (
     <>
       <h1 className="text-center m-4">Add Doctor</h1>
       <Container>
-        {
-        doctors.status === 201 ? (
+        {doctors.status === 201 ? (
           <Response show={show} setShow={setShow} />
         ) : (
-          <CreateDoctorForm />
-        )
-      }
+          <>
+            <CreateDoctorForm />
+            <Button variant="secondary" onClick={handleGoBack}>Go Back</Button>
+          </>
+        )}
       </Container>
     </>
   );
 }
-
 export default AddDoctor;
