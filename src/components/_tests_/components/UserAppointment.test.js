3b92ff546f401816ae275e7f266d356f3f988101
Tamunokuro <tamunokrobluejack@gmail.com>
2023-04-26 08:47:52 +0100

    Fix linters

--- src/components/_tests_/components/UserAppointment.test.js
+++ src/components/_tests_/components/UserAppointment.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
