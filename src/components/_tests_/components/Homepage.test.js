0c68f33b72d27c7e8e26ad2acf39b0dc2ea6ea14
Tamunokuro <tamunokrobluejack@gmail.com>
2023-04-26 08:34:32 +0100

    Fix linters

--- src/components/_tests_/components/Homepage.test.js
+++ src/components/_tests_/components/Homepage.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from  '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -7,12 +7,14 @@ import store from '../../../redux/configureStore';
 import Homepage from '../../Homepage/Homepage';
 
 describe('Home page', () => {
-    it('renders text in home page', () => {
-        render(<Provider store={store}>
-            <Router>
-                <Homepage />
-            </Router>
-        </Provider>);
-        expect(screen.getByText('Top-rated Doctors')).toBeInTheDocument();
-    });
-})
+  it('renders text in home page', () => {
+    render(
+      <Provider store={store}>
+        <Router>
+          <Homepage />
+        </Router>
+      </Provider>,
+    );
+    expect(screen.getByText('Top-rated Doctors')).toBeInTheDocument();
+  });
+});
