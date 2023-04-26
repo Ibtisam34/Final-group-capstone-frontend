85ed411b670d7c4ede99e4698c70ca6b6f697904
Tamunokuro <tamunokrobluejack@gmail.com>
2023-04-26 06:12:59 +0100

    Fix linter errors

--- src/__tests__/Login.test.js
+++ src/__tests__/Login.test.js
@@ -4,8 +4,7 @@ import '@testing-library/jest-dom/extend-expect';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import store from '../redux/configureStore';
-import Login from '../components/LoginRegister/Login'
-
+import Login from '../components/LoginRegister/Login';
 
 describe('Login to app', () => {
   it('renders correctly', () => {
@@ -13,22 +12,25 @@ describe('Login to app', () => {
     expect(tree).toMatchSnapshot();
   });
 
-  
   it('should have email input field', () => {
-    const { getByPlaceholderText } = render(<Provider store={store}><Router><Login /></Router></Provider>);
+    const { getByPlaceholderText } = render(
+      <Provider store={store}>
+        <Router><Login /></Router>
+      </Provider>,
+    );
     const emailInput = getByPlaceholderText('Email address');
     expect(emailInput).toBeInTheDocument();
-  })
+  });
 
   it('should have login button', () => {
     const { getByText } = render(<Provider store={store}><Router><Login /></Router></Provider>);
     const loginButton = getByText('Login');
     expect(loginButton).toBeInTheDocument();
-    })
+  });
 
-    it('should have register button', () => {
-        const { getByText } = render(<Provider store={store}><Router><Login /></Router></Provider>);
-        const registerButton = getByText('Register');
-        expect(registerButton).toBeInTheDocument();
-        })
+  it('should have register button', () => {
+    const { getByText } = render(<Provider store={store}><Router><Login /></Router></Provider>);
+    const registerButton = getByText('Register');
+    expect(registerButton).toBeInTheDocument();
+  });
 });
--- src/__tests__/Register.test.js
+++ src/__tests__/Register.test.js
@@ -12,28 +12,35 @@ describe('Register to app', () => {
     expect(tree).toMatchSnapshot();
   });
 
-    it('should have email input field', () => {
-        const { getByPlaceholderText } = render(<Provider store={store}><Router><Register /></Router></Provider>);
-        const emailInput = getByPlaceholderText('Email address');
-        expect(emailInput).toBeInTheDocument();
-    });
+  it('should have email input field', () => {
+    const { getByPlaceholderText } = render(
+      <Provider store={store}>
+        <Router><Register /></Router>
+      </Provider>,
+    );
+    const emailInput = getByPlaceholderText('Email address');
+    expect(emailInput).toBeInTheDocument();
+  });
 
-    it('should have password input field', () => {
-        const { getByPlaceholderText } = render(<Provider store={store}><Router><Register /></Router></Provider>);
-        const passwordInput = getByPlaceholderText('Full name');
-        expect(passwordInput).toBeInTheDocument();
-    });
+  it('should have password input field', () => {
+    const { getByPlaceholderText } = render(
+      <Provider store={store}>
+        <Router><Register /></Router>
+      </Provider>,
+    );
+    const passwordInput = getByPlaceholderText('Full name');
+    expect(passwordInput).toBeInTheDocument();
+  });
 
-    it('should have register button', () => {
-        const { getByText } = render(<Provider store={store}><Router><Register /></Router></Provider>);
-        const registerButton = getByText('Register');
-        expect(registerButton).toBeInTheDocument();
-    });
+  it('should have register button', () => {
+    const { getByText } = render(<Provider store={store}><Router><Register /></Router></Provider>);
+    const registerButton = getByText('Register');
+    expect(registerButton).toBeInTheDocument();
+  });
 
-    
-    it('should have login button', () => {
-        const { getByText } = render(<Provider store={store}><Router><Register /></Router></Provider>);
-        const loginButton = getByText('Login');
-        expect(loginButton).toBeInTheDocument();
-    })
+  it('should have login button', () => {
+    const { getByText } = render(<Provider store={store}><Router><Register /></Router></Provider>);
+    const loginButton = getByText('Login');
+    expect(loginButton).toBeInTheDocument();
+  });
 });
