710be510bd4b8c8ed51e4a1625e14687f8f20112
Tamunokuro <tamunokrobluejack@gmail.com>
2023-04-26 08:32:25 +0100

    Add test for home page

---
+++ src/components/_tests_/components/Homepage.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render, screen } from  '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import store from '../../../redux/configureStore';
+import Homepage from '../../Homepage/Homepage';
+
+describe('Home page', () => {
+    it('renders text in home page', () => {
+        render(<Provider store={store}>
+            <Router>
+                <Homepage />
+            </Router>
+        </Provider>);
+        expect(screen.getByText('Top-rated Doctors')).toBeInTheDocument();
+    });
+})
